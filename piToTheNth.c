49520bdd93d63ad581cd2874adcd7a45b3f14604
ebluke <ethanluke.social@gmail.com>
2021-08-25 22:59:50 +1000

    Prime Factorisation

---
+++ piToTheNth.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//Find Pi to a specific decimal range
+
+	//get user input
+
+	//calucalate pi to that input
+
+	//print out
+
+	//prompt again
+
+	int input;
+
+	while(input != 0)
+	{
+		scanf("Enter a value %i : " &dp)
+		
+	}
+}
+
