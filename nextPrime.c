3144c3c5be61d6bbcbf22fc6d2b765e67425d10d
ebluke <ethanluke.social@gmail.com>
2021-08-26 23:09:42 +1000

    semi finished

---
+++ nextPrime.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+bool isPrime(int n)
+{
+	bool test = false;
+	for(int i = 2; i<=n/2; i++)
+	{
+		if(n%i==0)
+		{
+			test = false;
+		}
+		else
+
+
+
+
+
+
+int nextPrime(int n)
+{
+	//n doesnt need to be prime.
+	// just find what the next prime number is going to +infinity
+
+	//check if n is odd or even
+	if(n%2==0)
+	{
+		//if n is even, then i = n+1
+		for(int i = n+1; i< sqrt(n); i+2)
+		{
+			if(n
+
+		}
+	}
+
+}
+
+int main()
+{
+
+
+}
+
